abb1da1614f8b5f53a91b6312ed562fd31523fc0
Pratham <pratham@narayanalab.com>
2023-07-18 11:54:29 +0530

    18 Jul 2023

---
+++ pyplot_matplotlib.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+dist = [0,1,2,3,4,5]
+time = [0,2,4,6,8,10]
+plt.plot(dist,time, linestyle = '-.', marker = 'x')
+plt.xlabel('Distance in kilometer')
+plt.ylabel('Time in minute')
+plt.title('Graph of Distance Vs Time')
+plt.savefig('Line1.png') #png, jpeg, pdf, svg
+plt.show()
---
+++ pyplot_matplotlib_2.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+
+year = ['2020 - 21','2019 - 21','2021 - 22','2022 - 23']
+ns = [83.4,89.7,88.7,91.2]
+nk = [87.3,88.3,82.5,90.2]
+nfb = [90.2,89.0,83.7,93.5]
+
+plt.plot(year, ns, marker = 'o', label = 'Narayana - Siliguri')
+plt.plot(year, nk, marker = '*', label = 'Narayana - Kalimpong')
+plt.plot(year, nfb, marker = '^', label = 'Narayana - Fulbari')
+
+plt.title("Result Analysis of Class - XII")
+plt.xlabel("Year")
+plt.ylabel("Percentage")
+plt.legend()
+
+plt.savefig('Result.png')
+plt.show()
