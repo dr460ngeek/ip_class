c5ef94929a83136934dc3759e46d2a8a703191c1
Pratham <pratham@narayanalab.com>
2023-07-19 13:38:36 +0530

    Empty commit. Ignore

--- practice_day1.py
+++ practice_day1.py
@@ -6,4 +6,3 @@ s = pd.Series([1,2,3,4,5,6,7], index=['a','b','c','d','e','f','g'])
 print("The orignal series is:\n", s) 
 print("After using tail: \n", s.tail(3))
 print("After using head: \n", s.head(3))
-
